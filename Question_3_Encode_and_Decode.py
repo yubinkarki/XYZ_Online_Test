8a40d33835cf0f0cf61f4492fd9f3f206a652fb8
Yubin <iamyubinkarki@gmail.com>
2022-05-08 10:12:34 +0545

    Final value for decoded string. Complete mess.

--- Question_3_Encode_and_Decode.py
+++ Question_3_Encode_and_Decode.py
@@ -1,4 +1,5 @@
 def encoder(text_to_encode):
+    """Encodes a given string and returns the final value."""
     individual_letters = list(text_to_encode)  # Converting string and storing it as a list.
     unique_letters = []
     count_list = []
@@ -28,6 +29,7 @@ def encoder(text_to_encode):
 
 
 def decoder(text_to_decode):
+    """Decodes the provided string and returns the final value."""
     # list = [[8, "A"], [5, "a"], [3, "X"], [14, "M"]]
     content_list = []
     numbers_list = ["0","1","2","3","4","5","6","7","8","9"]
@@ -37,20 +39,25 @@ def decoder(text_to_decode):
     while i < len(text_to_decode):
         if text_to_decode[i] in numbers_list:
             if text_to_decode[i] and text_to_decode[i+1] in numbers_list:  # Check for another number after the current number.
-                join_str = text_to_decode[i] + (text_to_decode[i+1])  # Can only check for 2 digit numbers.
+                join_str = int(text_to_decode[i] + (text_to_decode[i+1]))  # Can only check for 2 digit numbers.
                 content_list.append(join_str)
                 text_to_decode = text_to_decode[:i+1] + text_to_decode[i+2:]
             else:
-                content_list.append(text_to_decode[i])
+                content_list.append(int(text_to_decode[i]))
         else:
             content_list.append(text_to_decode[i])
         i += 1
 
-    print(content_list)
+    for i in range(0, len(content_list), 2):  # Reducing value of numbers by 2 in every 2nd position.
+        content_list[i] -= 2
+        mixed_list += content_list[i] * content_list[i+1]
+
+    final_string = "".join(mixed_list)
+    return final_string
 
 
 myinputencode = "AAAAAAaaaXMMMMMMMMMMMM"
 myinputdecode = "8A5a3X14M"
 
-print(encoder(myinputencode))
-decoder(myinputdecode)
+print("Encoding 'AAAAAAaaaXMMMMMMMMMMMM':", encoder(myinputencode))
+print("Decoding '8A5a3X14M':", decoder(myinputdecode))
